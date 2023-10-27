af5c0587ecbced7999b1735a28682718ef9917b5
shak33 <maciej@poppek.pl>
2023-10-27 20:03:25 +0200

    Written tests for useUsers hook, added wrapper to jest config, so now we can easily import it from global var, updated global types

--- __tests__/hooks/useUsers.test.tsx
+++ __tests__/hooks/useUsers.test.tsx
@@ -1,17 +1,17 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import axios from 'axios';
-import { useCompanies } from '@/hooks/useCompanies';
+import { useUsers } from '@/hooks/useUsers';
 
 jest.mock('axios');
 
-describe('useCompanies', () => {
+describe('useUsers', () => {
   const { wrapper } = global;
 
-  it('should fetch companies successfully', async () => {
-    const data = [{ id: 1, name: 'Company 1' }, { id: 2, name: 'Company 2' }];
+  it('should fetch users successfully', async () => {
+    const data = [{ id: 1, name: 'User 1' }, { id: 2, name: 'User 2' }];
     (axios.get as jest.Mock).mockResolvedValueOnce({ data: { data } });
 
-    const { result } = renderHook(() => useCompanies(), { wrapper });
+    const { result } = renderHook(() => useUsers(), { wrapper });
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
@@ -21,10 +21,10 @@ describe('useCompanies', () => {
   });
 
   it('should handle errors', async () => {
-    const error = new Error('Failed to fetch companies');
+    const error = new Error('Failed to fetch users');
     (axios.get as jest.Mock).mockRejectedValueOnce(() => Promise.reject(error));
 
-    const { result } = renderHook(() => useCompanies(), { wrapper });
+    const { result } = renderHook(() => useUsers(), { wrapper });
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
